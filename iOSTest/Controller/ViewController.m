9ab00a61eee00733dc3962f034d6f24fa87e15cb
wuxi <wuxi@arashivision.com>
2018-01-11 16:11:20 +0800

    add block init view

--- iOSTest/Controller/ViewController.m
+++ iOSTest/Controller/ViewController.m
@@ -28,18 +28,7 @@ typedef void(^myBlock)();
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UIButton *pushButton = [UIButton buttonWithType:UIButtonTypeSystem];
-    pushButton.backgroundColor = [UIColor redColor];
-    pushButton.frame = CGRectMake(100, 100, 100, 100);
-    [pushButton addTarget:self action:@selector(push:) forControlEvents:UIControlEventTouchUpInside];
-    
-    [self.view addSubview:pushButton];
-    
-    SwipHintView * view = [[SwipHintView alloc] initWithHintImage:[UIImage imageNamed:@"guide_img_drag"] hintText:@"Switch template, and each block supports sliding adjustment angle, two-finger zoom"];
-    view.frame = CGRectMake(40, 254, 295, 178);
-    [self.view addSubview:view];
-    
-    
+    [self addClosureInitView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -49,6 +38,39 @@ typedef void(^myBlock)();
 - (void)push:(UIButton *)button{
     TimerViewController *controller = [[TimerViewController alloc] init];
     [self.navigationController pushViewController:controller animated:YES];
+}
+
+//MARK: - pushButton
+- (void)addPushButton{
+    UIButton *pushButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    pushButton.backgroundColor = [UIColor redColor];
+    pushButton.frame = CGRectMake(100, 100, 100, 100);
+    [pushButton addTarget:self action:@selector(push:) forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.view addSubview:pushButton];
+}
+//MARK: - ClosureInitScrollView
+- (void)addClosureInitView{
+    CGRect rect = CGRectMake(0, 64, self.view.bounds.size.width, 44);
+    ClosureInitScrollView * scrollView = [[ClosureInitScrollView alloc] initWithFrame:rect numberOfLabel:^NSInteger{
+        return 10;
+    } labelOfIndex:^UILabel * _Nonnull(NSInteger index) {
+        UILabel *label = [[UILabel alloc] init];
+        label.text = [NSString stringWithFormat:@"hello %li", (long)index];
+        //先设置大字体，再调用sizeTofit，最后在修改字体，就实现了大标签中的小字体
+//        label.font = [UIFont systemFontOfSize:12];
+//        [label sizeToFit];
+//        label.font = [UIFont systemFontOfSize:10];
+        return label;
+    }];
+    [self.view addSubview:scrollView];
+}
+
+//MARK: - SwipHintView
+- (void)initSwipHintView{
+    SwipHintView * view = [[SwipHintView alloc] initWithHintImage:[UIImage imageNamed:@"guide_img_drag"] hintText:@"Switch template, and each block supports sliding adjustment angle, two-finger zoom"];
+    view.frame = CGRectMake(40, 254, 295, 178);
+    [self.view addSubview:view];
 }
 
 //MARK: - timer带上block
---
+++ iOSTest/View/ClosureInitScrollView.swift
@@ -0,0 +1,46 @@
+//
+//  ClosureInitScrollView.swift
+//  iOSTest
+//
+//  Created by wuxi on 2018/1/11.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+import UIKit
+
+class ClosureInitScrollView: UIScrollView {
+
+    //这里通过闭包来返回标签数量和内容
+    //构造函数的重载，oc中没有重载，swift中可以
+    init(frame: CGRect, numberOfLabel: ()->Int, labelOfIndex: (_ index: Int)->UILabel) {
+        //swift中的构造函数不需要返回值
+        super.init(frame: frame)
+        //实例化scrollView，并指定大小和位置
+        //swift中省略self
+        backgroundColor = UIColor.lightGray
+        //执行闭包，并且获取标签数量
+        let count = numberOfLabel()
+        print("标签数量 \(count)")
+        
+        let margin:CGFloat = 8
+        var x = margin
+        //遍历count，知道标签内容，添加到scrollView
+        for i in 0..<count
+        {
+            let label = labelOfIndex(i)
+            label.frame = CGRect(x: x, y: 0, width: label.bounds.width, height: frame.height)
+            //添加到scrollview
+            addSubview(label)
+            //递增x
+            x += label.bounds.width
+        }
+        contentSize = CGSize(width: x+margin, height: frame.size.height)
+        
+        
+    }
+    //保证只用代码的形式写界面，一旦使用了storyboard或者xib就会报错
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
