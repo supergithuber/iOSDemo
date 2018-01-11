0437aa8833e41f2ebd16142610cb864f9c91a2c4
wuxi <wuxi@arashivision.com>
2018-01-11 14:32:19 +0800

    add image scale

---
+++ iOSTest/Category/UIImage+Scale.h
@@ -0,0 +1,16 @@
+//
+//  UIImage+Scale.h
+//  iOSTest
+//
+//  Created by wuxi on 2018/1/11.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Scale)
+
+//裁剪图像的指定区域
+-(UIImage*)getSubImage:(CGRect)rect;
+
+@end
---
+++ iOSTest/Category/UIImage+Scale.m
@@ -0,0 +1,27 @@
+//
+//  UIImage+Scale.m
+//  iOSTest
+//
+//  Created by wuxi on 2018/1/11.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import "UIImage+Scale.h"
+
+@implementation UIImage (Scale)
+
+-(UIImage*)getSubImage:(CGRect)rect
+{
+    CGImageRef subImageRef = CGImageCreateWithImageInRect(self.CGImage, rect);
+    CGRect smallBounds = CGRectMake(0, 0, CGImageGetWidth(subImageRef), CGImageGetHeight(subImageRef));
+    
+    UIGraphicsBeginImageContext(smallBounds.size);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    CGContextDrawImage(context, smallBounds, subImageRef);
+    UIImage* smallImage = [UIImage imageWithCGImage:subImageRef];
+    UIGraphicsEndImageContext();
+    
+    return smallImage;
+}
+
+@end
