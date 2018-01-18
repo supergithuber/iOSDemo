8baf969802c99d038033d27b7e8e4fd0027fa238
sivanWu <1984430988@qq.com>
2018-01-18 10:46:11 +0800

    添加声音按钮回调

---
+++ iOSDemo/Tools/WXVolumeButtonHandler.h
@@ -0,0 +1,16 @@
+//
+//  WXVolumeButtonHandler.h
+//  iOSDemo
+//
+//  Created by wuxi on 2018/1/18.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef void (^WXVolumnButtonBlock)(void);
+@interface WXVolumeButtonHandler : NSObject
+
++ (instancetype)volumnButtonHandlerWithUpBlock:(WXVolumnButtonBlock)upBlock downBlock:(WXVolumnButtonBlock)downBlock;
+
+@end
---
+++ iOSDemo/Tools/WXVolumeButtonHandler.m
@@ -0,0 +1,13 @@
+//
+//  WXVolumeButtonHandler.m
+//  iOSDemo
+//
+//  Created by wuxi on 2018/1/18.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import "WXVolumeButtonHandler.h"
+
+@implementation WXVolumeButtonHandler
+
+@end
