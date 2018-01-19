f66eb93b769bc35ac2f56223469aff76c04cd48e
sivanWu <1984430988@qq.com>
2018-01-19 11:58:06 +0800

    setsystem volume

--- iOSDemo/Tools/WXVolumeButtonHandler.m
+++ iOSDemo/Tools/WXVolumeButtonHandler.m
@@ -29,13 +29,20 @@ static CGFloat minVolume                    = 0.00001f;
 
 @implementation WXVolumeButtonHandler
 
+- (void)dealloc{
+    [self.audioSession removeObserver:self forKeyPath:sessionVolumeKeyPath];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [self.volumeView removeFromSuperview];
+}
 //MARK: - init
 - (instancetype)initWithUpBlock:(WXVolumeButtonBlock)upBlock downBlock:(WXVolumeButtonBlock)downBlock {
     if (self = [super init]){
         _upBlock = upBlock;
         _downBlock = downBlock;
         _isAppActive = YES;
-        
+        [self setupAudioSession];
+        //监听APP是否活跃
+        [self initNotification];
     }
     return self;
 }
@@ -62,10 +69,42 @@ static CGFloat minVolume                    = 0.00001f;
     }
     //KVO:outputVolume
     [_audioSession addObserver:self forKeyPath:sessionVolumeKeyPath options:NSKeyValueObservingOptionOld | NSKeyValueObservingOptionNew context:sessionContext];
+    
+}
+- (void)initNotification{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidChangeActive:) name:UIApplicationWillResignActiveNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidChangeActive:) name:UIApplicationDidBecomeActiveNotification object:nil];
     //这里不支持后台，APP退到后台以后，声音停止，回到前台后，重新设置为激活
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(audioSessionInterrupt:) name:AVAudioSessionInterruptionNotification object:nil];
 }
+- (void)disableSystemVolumeHUDWithCompletion:(void(^)())completion{
+    self.volumeView = [[MPVolumeView alloc] initWithFrame:CGRectMake(MAXFLOAT, MAXFLOAT, 0, 0)];
+    [[[[UIApplication sharedApplication] windows] firstObject] addSubview:self.volumeView];
+}
+- (void)setInitialVolume {
+    self.initialVolume = self.audioSession.outputVolume;
+    if (self.initialVolume > maxVolume){
+        self.initialVolume = maxVolume;
+    }else if (self.initialVolume < minVolume){
+        self.initialVolume = minVolume;
+    }
+}
 
+//MARK: - KVO
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
+    if (context == sessionContext){
+        
+    }else{
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+//MARK: - Notification
+- (void)applicationDidChangeActive:(NSNotification *)notification {
+    self.isAppActive = [notification.name isEqualToString:UIApplicationDidBecomeActiveNotification];
+    if (_isAppActive){
+        
+    }
+}
 - (void)audioSessionInterrupt:(NSNotification *)notification{
     NSDictionary *interuptionDict = notification.userInfo;
     NSInteger interuptionType = [[interuptionDict valueForKey:AVAudioSessionInterruptionTypeKey] integerValue];
@@ -85,12 +124,16 @@ static CGFloat minVolume                    = 0.00001f;
             break;
     }
 }
-//MARK: - KVO
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
-    if (context == sessionContext){
-        
-    }else{
-        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+//MARK: - setSystemVolume
+- (void)setSystemVolume:(CGFloat)volume{
+    UISlider *volumeViewSlider = nil;
+    for (UIView *view in self.volumeView.subviews) {
+        if ([view.class.description isEqualToString:@"MPVolumeSlider"]){
+            volumeViewSlider = (UISlider *)view;
+            break;
+        }
     }
+    [volumeViewSlider setValue:volume animated:NO];
+    [volumeViewSlider sendActionsForControlEvents:UIControlEventTouchUpInside];
 }
 @end
