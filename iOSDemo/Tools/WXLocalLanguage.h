5126a3abaf5f00bfa728488caf6f22ae4cc706cf
sivanWu <1984430988@qq.com>
2018-01-26 16:12:55 +0800

    add localize

---
+++ iOSDemo/Tools/WXLocalLanguage.h
@@ -0,0 +1,17 @@
+//
+//  WXLocalLanguage.h
+//  iOSDemo
+//
+//  Created by wuxi on 2018/1/26.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WXLocalLanguage : NSObject
+
+@property (nonatomic, assign)BOOL isChinese;
+@property (nonatomic, assign)BOOL isChineseSimplified;
+@property (nonatomic, assign)BOOL isJapanese;
+
+@end
---
+++ iOSDemo/Tools/WXLocalLanguage.m
@@ -0,0 +1,20 @@
+//
+//  WXLocalLanguage.m
+//  iOSDemo
+//
+//  Created by wuxi on 2018/1/26.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import "WXLocalLanguage.h"
+
+@interface WXLocalLanguage()
+
+@property (nonatomic, copy)NSString* currentLanguage;
+@property (nonatomic, copy)NSString* currentRegionCode;
+
+@end
+@implementation WXLocalLanguage
+
+
+@end
