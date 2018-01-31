d7604353c741298f0b0b2cfd98e163e0fd998eed
sivanWu <1984430988@qq.com>
2018-01-31 18:52:50 +0800

    add artext

---
+++ iOSDemo/Model/WXARText.h
@@ -0,0 +1,13 @@
+//
+//  WXARText.h
+//  iOSDemo
+//
+//  Created by wuxi on 2018/1/31.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import <SceneKit/SceneKit.h>
+
+@interface WXARText : SCNText
+
+@end
---
+++ iOSDemo/Model/WXARText.m
@@ -0,0 +1,13 @@
+//
+//  WXARText.m
+//  iOSDemo
+//
+//  Created by wuxi on 2018/1/31.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import "WXARText.h"
+
+@implementation WXARText
+
+@end
