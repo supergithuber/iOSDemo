5e83db081ad6544c2bfc860945e89252c2a1540e
sivanWu <1984430988@qq.com>
2018-02-24 17:14:27 +0800

    add qrcode from album

---
+++ iOSDemo/Manager/QRCodeManager/WXQRCodeAlbumManager.h
@@ -0,0 +1,15 @@
+//
+//  WXQRCodeAlbumManager.h
+//  iOSDemo
+//
+//  Created by wuxi on 2018/2/24.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WXQRCodeAlbumManager : NSObject
+
++ (instancetype)sharedInstance;
+
+@end
---
+++ iOSDemo/Manager/QRCodeManager/WXQRCodeAlbumManager.m
@@ -0,0 +1,17 @@
+//
+//  WXQRCodeAlbumManager.m
+//  iOSDemo
+//
+//  Created by wuxi on 2018/2/24.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import "WXQRCodeAlbumManager.h"
+
+@interface WXQRCodeAlbumManager()
+
+@end
+
+@implementation WXQRCodeAlbumManager
+
+@end
