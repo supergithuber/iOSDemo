a3427be0948592919f0140d95bf68738c503ae76
sivanWu <1984430988@qq.com>
2018-03-06 17:33:31 +0800

    add bezierpath

--- iOSDemo/Controller/DemoViewController/WXBezierViewController.m
+++ iOSDemo/Controller/DemoViewController/WXBezierViewController.m
@@ -19,6 +19,7 @@
     self.view.backgroundColor = [UIColor whiteColor];
     [self drawLine];
     [self drawShape];
+    [self drawOvalInRect];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -38,11 +39,39 @@
     shapeLayer.strokeColor = [UIColor greenColor].CGColor;
     shapeLayer.fillColor = nil;
     
-    [self.view.layer addSublayer:shapeLayer];//带上颜色
+    [self.view.layer addSublayer:shapeLayer];//这样会带上颜色
 //    self.view.layer.mask = shapeLayer;
     
 }
 - (void)drawShape{
+    UIBezierPath *linePath = [UIBezierPath bezierPath];
+    linePath.lineCapStyle = kCGLineCapRound;
+    linePath.lineJoinStyle = kCGLineJoinRound;
+    [linePath moveToPoint:CGPointMake(200, 120)];
+    [linePath addLineToPoint:CGPointMake(300, 120)];
+    [linePath addLineToPoint:CGPointMake(250, 180)];
+    [linePath closePath];
     
+    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = linePath.CGPath;
+    shapeLayer.lineWidth = 5;
+    shapeLayer.strokeColor = [UIColor redColor].CGColor;
+    shapeLayer.fillColor = nil;
+    
+    [self.view.layer addSublayer:shapeLayer];
+}
+- (void)drawOvalInRect{
+    UIView *view = [[UIView alloc] initWithFrame:CGRectMake(20, 320, 150, 100)];
+    view.backgroundColor = [UIColor orangeColor];
+    [self.view addSubview:view];
+    UIBezierPath *path = [UIBezierPath bezierPathWithOvalInRect:view.bounds];
+    
+    CAShapeLayer *shapeLayer = [CAShapeLayer layer];
+    shapeLayer.path = path.CGPath;
+    shapeLayer.lineWidth = 8;
+    shapeLayer.strokeColor = [UIColor greenColor].CGColor;
+    shapeLayer.fillColor = nil;
+    
+    [view.layer addSublayer:shapeLayer];
 }
 @end
