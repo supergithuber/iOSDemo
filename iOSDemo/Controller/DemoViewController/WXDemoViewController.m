31bd376a72be34b56e6bcc086bb338a483e9bc6a
sivanWu <1984430988@qq.com>
2018-03-29 15:14:13 +0800

    temp close scrolling

--- iOSDemo/Controller/DemoViewController/WXDemoViewController.m
+++ iOSDemo/Controller/DemoViewController/WXDemoViewController.m
@@ -59,10 +59,10 @@ typedef void(^myBlock)();
     WXCyclicScrollLabel *flipLabel = [WXCyclicScrollLabel scrollLabelWithTitle:@"免费，不要钱，快来抢，带刀来，随便拿 ,快来看啊，难得的机会啊，我的存在就是为了凑字数，字数多一点就可以换行了" type:WXScrollTypeFlipRepeat velocity:1 options:UIViewAnimationOptionCurveEaseInOut font:[UIFont systemFontOfSize:12] scrolllSpace:10 insets:UIEdgeInsetsMake(0, 10 , 0, 10)];
     flipLabel.frame = CGRectMake(10, 200, 300, 30);
     flipLabel.backgroundColor = [UIColor blueColor];
-    flipLabel.scrollTitleColor = [UIColor whiteColor];
+    flipLabel.scrollTitleColor = [UIColor redColor];
     flipLabel.scrollLabelDelegate = self;
     [self.view addSubview:flipLabel];
-    [flipLabel beginScrolling];
+//    [flipLabel beginScrolling];
     
 //    WXCyclicScrollLabel *flipNoRepeatLabel = [WXCyclicScrollLabel scrollLabelWithTitle:@"免费，不要钱，快来抢，带刀来，随便拿 ,快来看啊，难得的机会啊，我的存在就是为了凑字数，字数多一点就可以换行了" type:WXScrollTypeFlipNoRepeat velocity:1 options:UIViewAnimationOptionCurveEaseInOut font:[UIFont systemFontOfSize:12] scrolllSpace:10 insets:UIEdgeInsetsMake(0, 10 , 0, 10)];
 //    flipNoRepeatLabel.frame = CGRectMake(10, 250, 300, 30);
