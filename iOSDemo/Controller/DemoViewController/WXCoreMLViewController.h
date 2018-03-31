b4442fdbd63fd7e28c9c2adf56d59685bbded0e1
sivanWu <1984430988@qq.com>
2018-03-31 15:00:08 +0800

    file move and add mlmodel

---
+++ iOSDemo/Controller/DemoViewController/WXCoreMLViewController.h
@@ -0,0 +1,13 @@
+//
+//  WXCoreMLViewController.h
+//  iOSDemo
+//
+//  Created by wuxi on 2018/3/31.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXCoreMLViewController : UIViewController
+
+@end
---
+++ iOSDemo/Controller/DemoViewController/WXCoreMLViewController.m
@@ -0,0 +1,30 @@
+//
+//  WXCoreMLViewController.m
+//  iOSDemo
+//
+//  Created by wuxi on 2018/3/31.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import "WXCoreMLViewController.h"
+
+@interface WXCoreMLViewController ()
+
+@end
+
+@implementation WXCoreMLViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+    
+}
+
+
+
+@end
