5ee3e2861874491a3ec1e452afd88024542e1ff2
sivanWu <wusi@insta360.com>
2018-04-09 17:28:14 +0800

    prepare webbrowser

---
+++ iOSDemo/Tools/WKWebView/WXWebBrowser.h
@@ -0,0 +1,13 @@
+//
+//  WXWebBrowser.h
+//  iOSDemo
+//
+//  Created by Wuxi on 2018/4/9.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WXWebBrowser : UIViewController
+
+@end
---
+++ iOSDemo/Tools/WKWebView/WXWebBrowser.m
@@ -0,0 +1,28 @@
+//
+//  WXWebBrowser.m
+//  iOSDemo
+//
+//  Created by Wuxi on 2018/4/9.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import "WXWebBrowser.h"
+
+@interface WXWebBrowser ()
+
+@end
+
+@implementation WXWebBrowser
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
