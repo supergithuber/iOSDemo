d423345e0d93830687ec15499268250dc8d8c4f8
sivanWu <wusi@insta360.com>
2018-04-10 17:13:37 +0800

    add some init funtion

--- iOSDemo/Tools/WKWebView/WXWebBrowser.m
+++ iOSDemo/Tools/WKWebView/WXWebBrowser.m
@@ -9,7 +9,9 @@
 #import "WXWebBrowser.h"
 #import <WebKit/WebKit.h>
 
-@interface WXWebBrowser ()
+static NSString *const kScriptMessageHandlerFirstKey = @"com.iOSDemo.scriptMessageHandlerFirstKey";
+
+@interface WXWebBrowser ()<WKScriptMessageHandler, WKUIDelegate, WKNavigationDelegate>
 
 @property (nonatomic, strong)WKWebView *wkWebView;
 //进度条
@@ -29,8 +31,10 @@
     
     if (_isNavigationHidden){
         self.navigationController.navigationBarHidden = NO;
-        //加一个自定义的navigationBar
-        
+        //加一个自定义的状态栏
+        UIView *statusBarView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, [[UIApplication sharedApplication] statusBarFrame].size.height)];
+        statusBarView.backgroundColor=[UIColor whiteColor];
+        [self.view addSubview:statusBarView];
     }else{
         self.navigationController.navigationBarHidden = NO;
     }
@@ -42,6 +46,22 @@
 //MARK: - 一些懒加载
 - (WKWebView *)wkWebView{
     if (!_wkWebView){
+        WKWebViewConfiguration *configuration = [WKWebViewConfiguration new];
+        configuration.allowsInlineMediaPlayback = YES;  //允许在线播放视频
+        configuration.allowsAirPlayForMediaPlayback = YES;  //是否允许AirPlay
+        configuration.selectionGranularity = WKSelectionGranularityCharacter;  //选择粒度
+        configuration.processPool = [WKProcessPool new];  //https://developer.apple.com/documentation/webkit/wkprocesspool
+        configuration.suppressesIncrementalRendering = YES;  //（压制增量渲染）是否全部加载到内存里，才会去渲染
+        //添加脚本信息处理者，需要实现WKScriptMessageHandler这个协议
+        WKUserContentController *contentController = [[WKUserContentController alloc] init];
+        [contentController addScriptMessageHandler:self name:kScriptMessageHandlerFirstKey];
+        configuration.userContentController = contentController;
+        
+        _wkWebView = [[WKWebView alloc] initWithFrame:self.view.bounds configuration:configuration];
+        
+        _wkWebView.UIDelegate = self;
+        _wkWebView.navigationDelegate = self;
+        _wkWebView.allowsBackForwardNavigationGestures = YES; //允许手势滑动前进后退
         
     }
     return _wkWebView;
@@ -50,10 +70,9 @@
     if (!_progressView){
         _progressView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleDefault];
         if (_isNavigationHidden){
-            //FIXME: 先不考虑iPhone X
-            _progressView.frame = CGRectMake(0, 20, self.view.bounds.size.width, 3);
+            _progressView.frame = CGRectMake(0, [[UIApplication sharedApplication] statusBarFrame].size.height, self.view.bounds.size.width, 3);
         }else{
-            _progressView.frame = CGRectMake(0, 64, self.view.bounds.size.width, 3);
+            _progressView.frame = CGRectMake(0, [[UIApplication sharedApplication] statusBarFrame].size.height + 44, self.view.bounds.size.width, 3);
         }
         // 设置进度条的色彩
         [_progressView setTrackTintColor:[UIColor colorWithRed:240.0/255 green:240.0/255 blue:240.0/255 alpha:1.0]];
