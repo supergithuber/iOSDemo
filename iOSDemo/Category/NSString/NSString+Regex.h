727f6a69f88e648ebccde5ef9e909188bf6a4fd2
sivanWu <1984430988@qq.com>
2018-05-08 14:47:27 +0800

    finish nsstring regex

--- iOSDemo/Category/NSString/NSString+Regex.h
+++ iOSDemo/Category/NSString/NSString+Regex.h
@@ -7,10 +7,38 @@
 //
 
 #import <Foundation/Foundation.h>
+//预先定义好的一些正则，都是指包含一个或者多个
+typedef NS_OPTIONS(NSUInteger, WXRegexType) {
+    WXRegexTypeNone        = 0 << 0,
+    WXRegexTypeCharacter   = 1 << 0,  //字母
+    WXRegexTypeDigital     = 1 << 1,  //数字
+    WXRegexTypeUnderline   = 1 << 2,  //下划线
+    WXRegexTypeChinese     = 1 << 3,  //汉字
+    WXRegexTypeSpace       = 1 << 4,  //空格
+    WXRegexTypeDot         = 1 << 5,  //点
+    WXRegexTypeAt          = 1 << 6,  //@
+};
 
 @interface NSString (Regex)
 
 
+/**
+ 空字符串返回false
+ @param type 类型
+ @return 是否符合
+ */
+- (BOOL)regexMatchWithWXRegexType:(WXRegexType)type;
+
+
+/**
+ 空字符串返回false，限制最大最小长度常常会在密码限制这种需求里用到，如果要穿最大，就用：NSUIntegerMax
+
+ @param type WXRegexType
+ @param minLength 最小长度
+ @param maxLength 最大长度
+ @return 是否符合
+ */
+- (BOOL)regexMatchWithWXRegexType:(WXRegexType)type minLength:(NSUInteger)minLength maxLength:(NSUInteger)maxLength;
 /**
  基本函数，用谓词表达式
  
--- iOSDemo/Category/NSString/NSString+Regex.m
+++ iOSDemo/Category/NSString/NSString+Regex.m
@@ -10,8 +10,59 @@
 
 @implementation NSString (Regex)
 
+//MARK: - public
+
+- (BOOL)regexMatchWithWXRegexType:(WXRegexType)type{
+    if (type == WXRegexTypeNone) {
+        return NO;
+    }
+    if (self.length == 0){
+        return NO;
+    }
+    NSString *regexString = [self wx_regexStringWithWXRegexType:type];
+    return [self regexMatchWithPattern:regexString];
+}
+
+- (BOOL)regexMatchWithWXRegexType:(WXRegexType)type minLength:(NSUInteger)minLength maxLength:(NSUInteger)maxLength{
+    if (self.length < minLength){
+        return NO;
+    }
+    if (self.length > maxLength){
+        return NO;
+    }
+    return [self regexMatchWithWXRegexType:type];
+}
+
 - (BOOL)regexMatchWithPattern:(NSString *)regex{
     NSPredicate *pred = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
     return [pred evaluateWithObject:self];
+}
+
+//MARK: - private
+- (NSString *)wx_regexStringWithWXRegexType:(WXRegexType)type{
+    NSString *regex = @"";
+    if (type & WXRegexTypeCharacter){
+        regex = [NSString stringWithFormat:@"%@a-zA-Z", regex];
+    }
+    if (type & WXRegexTypeDigital){
+        regex = [NSString stringWithFormat:@"%@\\d", regex];
+    }
+    if (type & WXRegexTypeUnderline){
+        regex = [NSString stringWithFormat:@"%@_", regex];
+    }
+    if (type & WXRegexTypeChinese){
+        regex = [NSString stringWithFormat:@"❶➋➌➍➎➏➐➑➒❿%@\u4e00-\u9fa5", regex];
+    }
+    if (type & WXRegexTypeSpace){
+        regex = [NSString stringWithFormat:@"%@ ", regex];
+    }
+    if (type & WXRegexTypeDot){
+        regex = [NSString stringWithFormat:@"%@.", regex];
+    }
+    if (type & WXRegexTypeAt){
+        regex = [NSString stringWithFormat:@"%@@", regex];
+    }
+    regex = [NSString stringWithFormat:@"^[%@]+$", regex];
+    return regex;
 }
 @end
