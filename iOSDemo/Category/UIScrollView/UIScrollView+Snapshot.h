3ac5c42f05111accdf12a116e0076de3f1e24c32
sivanWu <1984430988@qq.com>
2018-06-08 18:37:10 +0800

    udd scrollview category

---
+++ iOSDemo/Category/UIScrollView/UIScrollView+Snapshot.h
@@ -0,0 +1,15 @@
+//
+//  UIScrollView+Snapshot.h
+//  iOSDemo
+//
+//  Created by Wuxi on 2018/6/8.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (Snapshot)
+
+- (UIImage *)fullSnapshot;
+
+@end
---
+++ iOSDemo/Category/UIScrollView/UIScrollView+Snapshot.m
@@ -0,0 +1,20 @@
+//
+//  UIScrollView+Snapshot.m
+//  iOSDemo
+//
+//  Created by Wuxi on 2018/6/8.
+//  Copyright © 2018年 Wuxi. All rights reserved.
+//
+
+#import "UIScrollView+Snapshot.h"
+
+@implementation UIScrollView (Snapshot)
+
+//- (UIImage *)fullSnapshot{
+//    //先记录下自己的frame和contentoffset，然后扩大frame到contentSize，截图然后恢复
+//    CGRect currentFrame = self.frame;
+//    CGPoint currentContentOffset = self.contentOffset;
+//
+//
+//}
+@end
