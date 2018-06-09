f307c4e913dd485e17d5eb18d554f7d578618ffa
sivanWu <1984430988@qq.com>
2018-06-09 11:11:18 +0800

    update scrollview category

--- iOSDemo/Category/UIScrollView/UIScrollView+Snapshot.m
+++ iOSDemo/Category/UIScrollView/UIScrollView+Snapshot.m
@@ -10,11 +10,21 @@
 
 @implementation UIScrollView (Snapshot)
 
-//- (UIImage *)fullSnapshot{
-//    //先记录下自己的frame和contentoffset，然后扩大frame到contentSize，截图然后恢复
-//    CGRect currentFrame = self.frame;
-//    CGPoint currentContentOffset = self.contentOffset;
-//
-//
-//}
+- (UIImage *)fullSnapshot{
+    //先记录下自己的frame和contentoffset，然后扩大frame到contentSize，截图然后恢复
+    CGRect currentFrame = self.frame;
+    CGPoint currentContentOffset = self.contentOffset;
+
+    self.contentOffset = CGPointZero;
+    self.frame = CGRectMake(0, 0, self.contentSize.width, self.contentSize.height);
+    
+    UIGraphicsBeginImageContextWithOptions(self.contentSize, YES, 0);//默认缩放因子
+    [self.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    self.contentOffset = currentContentOffset;
+    self.frame = currentFrame;
+    return image;
+}
 @end
