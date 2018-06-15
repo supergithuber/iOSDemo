bb7ad7b5eb1f0a9f842f3769bccff273b386fe1e
sivanWu <1984430988@qq.com>
2018-06-15 16:54:15 +0800

    update lock

--- iOSDemo/Controller/ViewController/WXLockViewController.m
+++ iOSDemo/Controller/ViewController/WXLockViewController.m
@@ -13,6 +13,11 @@
 @property (nonatomic, strong)NSLock *lock;
 @property (nonatomic, strong)NSRecursiveLock *recursiveLock;
 
+@property (nonatomic, strong)NSCondition *condition;
+@property (nonatomic, strong)NSMutableArray *conditionArray;
+
+@property (nonatomic, strong)NSConditionLock *conditionLock;
+
 @end
 
 @implementation WXLockViewController
@@ -34,7 +39,7 @@
     
     [self.lock unlock];
 }
-
+//MARK: NSRecursiveLock
 - (void)wx_recursiveLock:(int)value{
     [self.recursiveLock lock];
     if (value != 0){
@@ -44,10 +49,29 @@
     }
     [self.recursiveLock unlock];
 }
+//MARK: semaphore
 - (void)wx_semaphore{
     //参考SemaphoreViewController
 }
-
+//MARK: semaphore
+- (void)wx_condition{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self.conditionLock lock];
+        while (self.conditionArray.count == 0) {
+            [self.condition wait]; //在等待其他线程来的信号
+            
+        }
+        [self.conditionArray removeObjectAtIndex:0];
+        [self.conditionLock unlock];
+    });
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self.conditionLock lock];
+        [self.conditionArray addObject:[NSObject new]];
+        NSLog(@"添加了一个东西");
+        [self.condition signal];  //通知其他线程
+        [self.conditionLock unlock];
+    });
+}
 //MARK: GET
 - (NSLock *)lock{
     if (_lock){
@@ -60,6 +84,24 @@
         _recursiveLock = [[NSRecursiveLock alloc] init];
     }
     return _recursiveLock;
+}
+- (NSCondition *)condition{
+    if (_condition){
+        _condition = [[NSCondition alloc] init];
+    }
+    return _condition;
+}
+- (NSMutableArray *)conditionArray{
+    if (_conditionArray){
+        _conditionArray = [NSMutableArray array];
+    }
+    return _conditionArray;
+}
+- (NSConditionLock *)conditionLock{
+    if (_conditionLock){
+        _conditionLock = [[NSConditionLock alloc] init];
+    }
+    return _conditionLock;
 }
 
 @end
