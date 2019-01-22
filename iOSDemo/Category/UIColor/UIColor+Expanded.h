b2bf9eeb16d3882b9e97764bdf7e42d96ee5a3c9
sivanWu <1984430988@qq.com>
2019-01-22 18:18:32 +0800

    color

--- iOSDemo/Category/UIColor/UIColor+Expanded.h
+++ iOSDemo/Category/UIColor/UIColor+Expanded.h
@@ -18,6 +18,14 @@ NS_ASSUME_NONNULL_BEGIN
 + (UIColor *)wx_colorWithRGBHex:(UInt32)hex; //0x430234
 + (UIColor *)wx_colorWithHexString:(NSString *)stringToConvert;//"#ffffff", "0xffffff"
 
+/**
+ 取两个颜色之间的线性差值
+
+ @param ratio 从第一个颜色到第二个的百分比，【0.0， 1.0】，大于1取toColor，小于0取fromcColor
+ */
++ (UIColor *)wx_colorByLerpingFromColor:(UIColor *)fromColor
+                                toColor:(UIColor *)toColor
+                                  ratio:(CGFloat)ratio;
 @end
 
 NS_ASSUME_NONNULL_END
--- iOSDemo/Category/UIColor/UIColor+Expanded.m
+++ iOSDemo/Category/UIColor/UIColor+Expanded.m
@@ -49,4 +49,17 @@
     if (![scanner scanHexInt:&hexNum]) return nil;
     return [UIColor wx_colorWithRGBHex:hexNum];
 }
+
++ (UIColor *)wx_colorByLerpingFromColor:(UIColor *)fromColor
+                                toColor:(UIColor *)toColor
+                                  ratio:(CGFloat)ratio {
+    ratio = CLAMP(ratio, 0.f, 1.f);
+    const CGFloat *fromComponents = CGColorGetComponents(fromColor.CGColor);
+    const CGFloat *toComponents = CGColorGetComponents(toColor.CGColor);
+    float r = fromComponents[0] + ((toComponents[0] - fromComponents[0]) * ratio);
+    float g = fromComponents[1] + ((toComponents[1] - fromComponents[1]) * ratio);
+    float b = fromComponents[2] + ((toComponents[2] - fromComponents[2]) * ratio);
+    float a = fromComponents[3] + ((toComponents[3] - fromComponents[3]) * ratio);
+    return [UIColor colorWithRed:r green:g blue:b alpha:a];
+}
 @end
